11abc21071d055b0581f93e0772232256c212c95
Johannes Lindhart Borresen <jlb@specialminds.dk>
2021-06-17 13:55:41 +0200

    chore: Forgot to recompile.

--- dist/index.js
+++ dist/index.js
@@ -9955,7 +9955,7 @@ function run() {
                         env: Object.assign(Object.assign({}, process.env), { 
                             // Using this env var right now before the same option in rsync (-e)
                             // seems to be buggy when called with exec().
-                            RSYNC_RSH: `${bins.ssh} -o "UserKnownHostsFile=${knownhostsPath}" -o "IdentitiesOnly=yes" -i ${identityFilePath}` })
+                            RSYNC_RSH: `${bins.ssh} -o "UserKnownHostsFile=${knownhostsPath}" -o "PasswordAuthentication=no" -i ${identityFilePath}` })
                     });
                 });
             }
@@ -9965,7 +9965,7 @@ function run() {
                     env: Object.assign(Object.assign({}, process.env), { 
                         // Using this env var right now before the same option in rsync (-e)
                         // seems to be buggy when called with exec().
-                        RSYNC_RSH: `${bins.ssh} -o "UserKnownHostsFile=${knownhostsPath}" -o "IdentitiesOnly=yes" -i ${identityFile}` })
+                        RSYNC_RSH: `${bins.ssh} -o "UserKnownHostsFile=${knownhostsPath}" -o "PasswordAuthentication=no" -i ${identityFile}` })
                 });
             }
             if (returnCode != 0) {
